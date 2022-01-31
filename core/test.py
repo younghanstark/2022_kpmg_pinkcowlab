f90c756655addceedacfb11f3e1ed6ce9fe38ea9
younghan <younghanpark@yonsei.ac.kr>
2022-01-31 22:33:19 +0900

    Express server generated, Set basic API server
    Set Python shell, wrote test code

---
+++ core/test.py
@@ -0,0 +1,4 @@
+import sys
+
+inputString = sys.argv[1]
+print(''.join(reversed(inputString)))
---
+++ myapp/routes/api.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+
+var {PythonShell} = require('python-shell');
+var options = {
+    pythonPath: '',
+    scriptPath: '../core',
+    args: []
+};
+
+router.get('/:inputString', function(req, res) {
+    var inputString = req.params.inputString;
+    options.args[0] = inputString;
+    PythonShell.run('test.py', options, function(err, results) {
+        if (err) throw err;
+        res.status(200).json (
+            {
+                "reversed": results[0]
+            }
+        );
+    });
+});
+
+module.exports = router;
