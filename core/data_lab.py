5569c90677d7bc13c7ea079b0f78202c8d3e7291
younghan <younghanpark@yonsei.ac.kr>
2022-02-08 21:58:14 +0900

    added data tokenizing function

--- core/data_lab.py
+++ core/data_lab.py
@@ -6,6 +6,11 @@ data = rd.read_data('purchase_data_7Feb22')
 num_data = len(data.values)
 correct = 0
 for pair in data.values:
+    # print("From %s we got %s, intended %s" % (pair[0], sp.get_core(pair[0]), pair[1]))
     if sp.get_core(pair[0]) == pair[1]:
+        # print("Correct")
         correct += 1
-print(correct/num_data*100, "% Accuracy!")
+    else:
+        print("From %s we got %s, intended %s" % (pair[0], sp.get_core(pair[0]), pair[1]))
+        print("Not correct")
+print("%f percent accuracy!" % (correct/num_data*100))
--- core/string_processing.py
+++ core/string_processing.py
@@ -1,4 +1,16 @@
-import soynlp
+import read_data as rd
+from soynlp.tokenizer import MaxScoreTokenizer
+
+data = rd.read_data('purchase_data_7Feb22')
+scores = {}
+for pair in data.values:
+    scores[pair[1]] = 1.0
+tokenizer = MaxScoreTokenizer(scores=scores)
 
 def get_core(string):
-    return string
+    tokens = tokenizer.tokenize(string)
+    for token in tokens:
+        if token in scores:
+            return token
+    return "noun extractor"
+
